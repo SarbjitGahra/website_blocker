005d16e1438b7e0318314a2aa96ce7b19073aaaf
Sarbjit <sarbjit.gahra@gmail.com>
2017-10-07 04:31:53 -0400

    Second commit

--- website_blocker.py
+++ website_blocker.py
@@ -1,9 +1,10 @@
 #/etc/hosts
 import time
 from datetime import datetime as dt
+host_temp="hosts"
 host_path="/etc/hosts"
 redirect="127.0.0.1"
-wesite_list=["www.facebook.com" , "facebook.com" ,"www.instagram.com"]
+website_list=["www.facebook.com" , "facebook.com" ,"www.instagram.com"]
 
 year = dt.now().year
 month= dt.now().month
@@ -12,8 +13,24 @@ day = dt.now().day
 
 
 while True:
-    if dt(year , month , day , 1) < dt.now() < dt(year , month , day , 9):
+    if dt(year , month , day , 20) < dt.now() < dt(year , month , day , 5):
         print ("No distractions ")
+        with open(host_temp , 'r+' ) as file:
+            content = file.read()
+            print (content)
+            for website in website_list:
+                if website in content:
+                    continue
+                else:
+                    file.write(redirect + "  " + website + "\n")
     else:
+        with open(host_temp , 'r+' ) as file:
+            content = file.readlines()
+            file.seek(0)
+            for line in content:
+                if not any (website in line for website in website_list):
+                    file.write(line)
+            file.truncate()
         print ("fun hours ")
+
     time.sleep(5)
